b50a20293388e3bb3f0eae0ebdb8a885acf22a53
kaoru181206 <karats24.kaoru@gmail.com>
2023-09-28 18:51:00 +0900

    バックエンド（仮）からデータを取得しPostsListを表示するよう修正

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,12 +2,33 @@ import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from './PostsList.module.css';
 import Modal from "./Modal";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function PostsList({ isPosting, onStopPosting }) {
+    // postデータを保持
     const [posts, setPosts] = useState([]);
+    // 読込状態を保持
+    const [isFetching, setIsFetchin] = useState(false);
+
+    useEffect(() => {
+        async function fetchPosts() {
+            setIsFetchin(true);
+            const response = await fetch('http://localhost:8080/posts');
+            const resData = await response.json();
+            setPosts(resData.posts);
+            setIsFetchin(false);
+        }
+        fetchPosts();
+    }, []);
 
     function addPostHandler(postData) {
+        fetch('http://localhost:8080/posts', {
+            method: 'POST',
+            body: JSON.stringify(postData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
         setPosts((prevState) => [postData, ...prevState]);
     }
 
@@ -19,16 +40,19 @@ function PostsList({ isPosting, onStopPosting }) {
                 </Modal>
             }
 
-            {posts.length > 0 && 
+            {!isFetching && posts.length > 0 &&
                 <ul className={classes.posts}>
                     {posts.map(post => <Post key={post.body} author={post.author} body={post.body} />)}
                 </ul>}
 
-            {posts.length === 0 &&
-                <div style={{textAlign: 'center', color: 'white'}}>
+            {!isFetching && posts.length === 0 &&
+                <div style={{ textAlign: 'center', color: 'white' }}>
                     <h2>There are no posts yet.</h2>
                     <p>Start adding some!</p>
                 </div>}
+            
+            {isFetching && <div style={{textAlign: 'center', color: 'white'}}>
+                <p>Loading posts</p></div>}
         </>
 
     )
